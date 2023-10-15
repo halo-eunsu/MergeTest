b7b3d1da22c5a418f5966978b20221f5660bb34c
halo-eunsu <ujd017@naver.com>
2023-10-15 21:10:08 +0900

    1

--- src/main/java/com/nhnacademy/TestToMerge.java
+++ src/main/java/com/nhnacademy/TestToMerge.java
@@ -3,5 +3,6 @@ package com.nhnacademy;
 public class TestToMerge {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello world!");
     }
 }
